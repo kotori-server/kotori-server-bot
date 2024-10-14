5d788382e352ccd70111451dcc516eb68ba86958
kotori-server <fynk7777sub-kotorisab7@yahoo.co.jp>
2024-10-14 13:26:41 +0900

    起動ログが来るはず

--- main.py
+++ main.py
@@ -14,13 +14,27 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Botが起動したときのイベント
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print(f'{bot.user} としてログインしました')
     try:
         synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
+        print(f'Synced {len(synced)} commands')
     except Exception as e:
-        print(f"Error syncing commands: {e}")
+        print(f'Error syncing commands: {e}')
+    check_members.start()
+    await send_update_message()
+    await bot.change_presence(status=discord.Status.online, activity=discord.CustomActivity(name='ことり鯖'))
 
+
+async def send_update_message():
+    channel_id = 1295239928807948411
+    channel = await bot.fetch_channel(channel_id)
+    embed = discord.Embed(
+        title="BOTが起動しました！",
+        description="BOT has been started!",
+        color=0x00BFFF,
+        timestamp=datetime.now()
+    )
+    await channel.send(embed=embed)
 
 try:
     keep_alive()
