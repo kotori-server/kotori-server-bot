922e13a628a70be1d7b4cc07cdffb4d05f73cc98
kotori-server <fynk7777sub-kotorisab7@yahoo.co.jp>
2024-10-14 13:10:57 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import discord
+from discord import app_commands
+from keep_alive import keep_alive
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.all()
+
+# Botの準備
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Botが起動したときのイベント
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
+
+
+try:
+    keep_alive()
+    bot.run(TOKEN)
+except Exception as e:
+    print(f'エラーが発生しました: {e}')
